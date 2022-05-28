8681ba7c7e113705c1f6e19cdf1abdef7f6f021d
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-28 18:45:09 +0200

    preparazione-news(da-perfezionare)

--- src/components/NewsComponent.vue
+++ src/components/NewsComponent.vue
@@ -2,30 +2,31 @@
 
   <div class="news container-fluid">
 
-    <div class="container">
+    <div class="container py-5">
 
       <h6>our editorial content</h6>
-      <h1>latest news</h1>
+      <h1 class="my-4">latest news</h1>
+
       <div class="d-flex justify-content-between">
         <p>Every week we publish content about what is best in the business world</p> 
         <button>SEE ALL</button>
       </div>
 
-      <div class="row">
+      <div class="row py-5">
 
         <div class="col-4">
-          <h1>IMMAGINE</h1>
-          <p>Increasing creativity is possible for everyone</p>
+        <img src="../assets/images/news-1.jpg" alt="">
+          <h3 class="news-text-1">Increasing creativity is <br> possible for everyone</h3>
         </div>
         
         <div class="col-4">
-          <h1>IMMAGINE</h1>
-          <p>Because market research is part of the business plan</p>
+        <img src="../assets/images/news-2.jpg" alt="">
+          <h3 class="news-text-2">Because market <br> research is part of the <br> business plan</h3>
         </div>
         
         <div class="col-4">
-          <h1>IMMAGINE</h1>
-          <p>Working from home is now a trend</p>
+        <img src="../assets/images/news-3.jpg" alt="">
+          <h3 class="news-text-3">Working from home is <br> now a trend</h3>
         </div>
 
       </div>
@@ -46,25 +47,77 @@ export default {
 
 <style lang="scss" scoped>
 
-  .news {
-    background-color: #FF1111;
-    border: 1px solid #000000;
+.news {
+  background-color: #E7EEE9;
+
+  h6 {
+    text-transform: uppercase;
+    font-weight: bold;
+    color: #048383;
   }
 
-  .container {
-    background-color: #FFFFFF;
-    border: 1px solid #000000;
+  h1 {
+    text-transform: capitalize;
+    font-weight: bold;
+    font-size: 58px;
   }
 
-  .row {
-    background-color: #D9FF00;
-    border: 1px solid #000000;
+  p {
+    font-size: 19px;
+    color: #676669
   }
 
-  .col, .col-4 {
-    background-color: #00FF15;
-    border: 1px solid #000000;
+  button {
+    margin: 0 10px;
+    background-color: #048383;
+    color: #FFFFFF;
+    border: none;
+    border: 1px solid #048383;
+    border-radius: 5px;
+    padding: 8px 30px;
   }
+}
+
+.col-4 {
+
+  
+  img {
+  width: 100%;
+  border-radius: 5px;
+  position: relative;
+  background-color: #000000;
+  
+  }
+
+  h3 {
+    text-align: center;
+    color: #000000;
+    font-weight: bold;
+    
+  }
+
+  .news-text-1 {
+    position: absolute;
+    top: 700%;
+    left: 27%;
+    transform: translate(-50%, -50%);
+  }
+
+  .news-text-2 {
+    position: absolute;
+    top: 700%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .news-text-3 {
+    position: absolute;
+    top: 700%;
+    left: 73%;
+    transform: translate(-50%, -50%);
+  }
+}
+
 
 
 </style>
