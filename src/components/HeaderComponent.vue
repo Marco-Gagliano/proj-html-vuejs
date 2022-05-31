de6e1df2d2717cffc06e6e80a38701df48cf566d
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-31 17:17:44 +0200

    completato

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,18 +5,18 @@
     <!-- INFO -->
     <div class="info container-fluid">
       <div class="container">
-        <div class="row">
+        <div class="row py-3">
 
-          <div class="col-6">
+          <div class="col-6 opening-hours">
             <i class="fa-solid fa-clock"></i>
             <span> Open Hours: Mon - Sat - 9:00 - 18:00</span>
           </div>
 
-          <div class="col-6">
+          <div class="col-6 contacts">
             <nav>
               <ul>
                 <li><a href="#"><i class="fa-solid fa-phone"></i>+1 (305) 1234-5678</a></li>
-                <li><a href="#"><i class="fa-solid fa-envelope"></i>hello@examle.com</a></li>
+                <li><a href="#"><i class="fa-solid fa-envelope"></i>hello@example.com</a></li>
                 <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
                 <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
                 <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
@@ -36,8 +36,8 @@
         <div class="container">
           <div class="row">
 
-            <div class="col-4 mt-4">
-              <p class="text-light">NEXGEN</p>
+            <div class="col-2 mt-4">
+              <h5><span id="nex">NEX</span>GEN</h5>
             </div>
 
             <div class="col-7 mt-4">
@@ -45,11 +45,13 @@
               <ul v-for="(menu, index) in menuList" :key="`menu${index}`">
                 <li><a :href="menu.href">{{menu.name}}</a></li>
               </ul>
-
+              <ul>
+                <li><a href="#"><i class="fa-regular fa-user"></i></a></li>
+              </ul>
             </div>
 
-            <div class="col-1 mt-4">
-              <a href="#"><i class="fa-regular fa-user"></i></a>
+
+            <div class="col-2 mt-4">
               <button>GET IN TOUCH</button>
             </div>
 
@@ -63,8 +65,8 @@
       <div class="description text-center">
         <div class="row">
           <div class="col">
-            <h1 class="text-light fw-bold">Financial Risk</h1>
-            <p>The right outcomes depend on continuos rigor in governance, models, and processes across the finance function</p>
+            <h1 class="my-4">Financial Risk</h1>
+            <p>The right outcomes depend on continuos rigor in governance, models, and <br>processes across the finance function</p>
             <button>GET IN TOUCH</button>
             <button>READ MORE</button>  
           </div>
@@ -140,18 +142,35 @@ export default {
     height: 1055px;
   }
 
-  i {
-    margin: 0 20px;
-    color: $salt_box;
+  .contacts {
+    display: flex;    
+    justify-content: end;
+
+    i {
+      margin-left: 20px;
+      margin-right: 10px;
+      color: $salt_box;
+    }
+
+    p {
+      letter-spacing: 5px;
+      font-weight: bold;
+    }
   }
 
   span {
     color: $salt_box;
   }
 
-  p {
-    letter-spacing: 5px;
+  h5 {
+    color: $gray_nurse;
     font-weight: bold;
+    letter-spacing: 5px;
+    margin-left: 10px;
+
+    #nex {
+      @include bg-text4()
+    }
   }
 
   // /GENERAL PRESETS
@@ -160,9 +179,17 @@ export default {
   // INFO
   .info {
     background-color: $shark;
-    
+
+    .col-6 {
+      display: flex;
+      align-items: center;
+    }
   }
 
+  .opening-hours i {
+    margin-right: 8px ;
+    color: $salt_box;
+  }
   .info a {
     text-decoration: none;
     color: $salt_box;
@@ -171,22 +198,23 @@ export default {
   .info ul {
     display: flex;
     flex-direction: row;
+    margin: 0;
   }
   // /INFO
   
 
   // MENU
 
-
   .col-7 {
     display: flex;
     flex-direction: row;
     justify-content: end;
   }
-
-  .col-1 {
+  
+  .col-2 {
     display: flex;
     flex-direction: row;
+    justify-content: end;
   }
 
   .menu a {
@@ -195,7 +223,7 @@ export default {
     margin: 0 10px;
   }
 
-  .menu button {
+  button {
     @include button_type1();
   }
 
@@ -208,13 +236,23 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+    padding-top: 270px;
+
+    h1 {
+      font-size: 70px;
+      font-weight: bold;
+      color: $white;
+    }
 
     p {
       color: #BCBDBD;
+      font-size: 19px;
+      
     }
 
     button {
       @include button_type2();
+      margin: 0 10px;
     }
 
   }
